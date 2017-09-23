c4f931f5d1508a64ed5dd06b6fd7e13a9a7fa9ec
machade <matheus-machado1@hotmail.com>
2017-09-22 23:21:10 -0300

    Alteração no path das imagens

--- src/pages/cadastro-rota/cadastro-rota.ts
+++ src/pages/cadastro-rota/cadastro-rota.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Modal,ModalController} from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ModalController} from 'ionic-angular';
 import {ModalRotaPage} from '../modal-rota/modal-rota';
 
 /**
