9586793ca9cf763724aa2c08bf60439eac275af1
machade <matheus-machado1@hotmail.com>
2017-10-02 16:36:55 -0300

    Criação da provider e ajuste na configuração ionic

---
+++ src/providers/cadastro-rota.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the CadastroRota provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class CadastroRota {
+
+  constructor(public http: Http) {
+    console.log('Hello CadastroRota Provider');
+  }
+  getRotas(){
+    return this.http.get('http://localhost:3000/tipo_rota').map(res => res.json());
+  }
+
+}
