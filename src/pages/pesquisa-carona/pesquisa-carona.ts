c6634224872407afe5bf0a243b22f635264350d8
machade <matheus-machado1@hotmail.com>
2017-10-31 23:37:07 -0200

    atualização procurar carona

--- src/pages/pesquisa-carona/pesquisa-carona.ts
+++ src/pages/pesquisa-carona/pesquisa-carona.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CadastroRota } from '../../providers/cadastro-rota';
+import { } from 'googlemaps';
+import { MapsAPILoader } from '@agm/core';
 
 /**
  * Generated class for the PesquisaCaronaPage page.
@@ -15,11 +18,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PesquisaCaronaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  id:'';
+  private localizacao: Array<any> = [];
+  private dest_usuario: Array<any> = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public cadRota: CadastroRota, public mapsApiLoader: MapsAPILoader) {
+      this.getLocal();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PesquisaCaronaPage');
   }
 
+  getLocal() {
+    this.cadRota.getDestUsuario().subscribe(data => {
+      this.dest_usuario = data;
+      let local1=  new google.maps.LatLng( this.dest_usuario[0].localizacao.x, this.dest_usuario[0].localizacao.y);
+      let local2=  new google.maps.LatLng( this.dest_usuario[1].localizacao.x, this.dest_usuario[1].localizacao.y) ;
+
+      this.mapsApiLoader.load().then(() => {
+        let dist = google.maps.geometry.spherical.computeDistanceBetween(local1,local2);
+        console.log(dist);
+      });
+    });  
+  }
 }
